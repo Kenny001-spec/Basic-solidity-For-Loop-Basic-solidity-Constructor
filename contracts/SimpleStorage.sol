1209de9889afc7aa7a382ab707b10a538737bcb4
kenny001-spec <abubakarkehinde88@gmail.com>
2024-01-18 17:18:36 +0100

    Basic-Solidity-Mapping

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -8,8 +8,8 @@ contract SimpleStorage {
 
     // Much better way to create a list is to use Data structure called an Array
 
-
-
+    // BASIC SOLIDITY MAPPING
+    mapping(string => uint256) public nameToFAvoriteNumber; //Every single name is going to map to specific Number
 
 
     struct People {
@@ -26,13 +26,23 @@ contract SimpleStorage {
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
         // uint256 testVar = 5;    // just for the testing of the Deployed Contract
-        retrieve();
+        // retrieve();
     }
     // view, pure
     function retrieve() public  view returns(uint256) {
         return favoriteNumber;
     }
-  
+
+    // Function that can help people array
+    // CALL-DATA, MEMORY, STORAGE   // The variable only exist temporarily
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        // People memory newPerson = People({favoriteNumber: _favoriteNumber, name: _name});   // array of type people...by using bracket notation
+        // People memory newPerson = People()    // we can also declare it this way
+        people.push(People(_favoriteNumber, _name));     // we call a push function that's available on our object...it will push people in to our people array.
+        nameToFAvoriteNumber[_name] = _favoriteNumber;
+
+    }
+    
 }
 
 // 0xd9145CCE52D386f254917e481eB44e9943F39138
