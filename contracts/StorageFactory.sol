07b665fa8fb330d473ae94f927d12cd5cb5b1f71
kenny001-spec <abubakarkehinde88@gmail.com>
2024-01-19 10:52:25 +0100

    ways to use multiple file in a particular contract

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,55 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SimpleStorage {
-    // This gets initialized to zero!
-    uint256 public favoriteNumber;
-    // People public person = People({favoriteNumber: 2, name: "patrick"});
+import "./SimpleStorage.sol";
 
-    // Much better way to create a list is to use Data structure called an Array
-
-    // BASIC SOLIDITY MAPPING
-    mapping(string => uint256) public nameToFAvoriteNumber; //Every single name is going to map to specific Number
-
-    // Create a new Type in solidity called struct.
-    struct People {
-        uint256 favoriteNumber; // this is at index 0
-        // uint256 brothersNumber; // this is at index 1
-        // uint256 sistersNumber; // this is at index 2
-        string name;
-    }
-
-    // uint256[] public favoriteNumberList; // Numbers list is going to be an array.
-    People[] public  people;
-    // 0:2, Patrick, 1: 7, Jon
-
-    function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
-        // uint256 testVar = 5;    // just for the testing of the Deployed Contract
-        // retrieve();
-    }
-    // view, pure
-    function retrieve() public  view returns(uint256) {
-        return favoriteNumber;
-    }
-
-    // Function that can help people array
-    // CALL-DATA, MEMORY, STORAGE   // The variable only exist temporarily
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        // People memory newPerson = People({favoriteNumber: _favoriteNumber, name: _name});   // array of type people...by using bracket notation
-        // People memory newPerson = People()    // we can also declare it this way
-        people.push(People(_favoriteNumber, _name));     // we call a push function that's available on our object...it will push people in to our people array.
-        nameToFAvoriteNumber[_name] = _favoriteNumber;
-
-    }
-    
-}
-
+    // Single file solidity can hold multiple contract
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    // Type Visibilty variable
+    SimpleStorage[] public simpleStorageArray;
 
     function createSimpleStorageContract() public {
         // How does storage factory know what simple storage looks like?
-        simpleStorage = new SimpleStorage();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
     }
 }
