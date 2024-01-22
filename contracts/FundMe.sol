7d8193a7db3972d0b7c59abe4d9a59965d7e7831
kenny001-spec <abubakarkehinde88@gmail.com>
2024-01-22 11:43:38 +0100

    Floating Point Math in Silidity

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,17 +5,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+
 contract FundMe  {
     
-
+    uint256 public minimumUsd = 50;
     function fund() public payable {
         // We want to be able to set a minimum fund amout in UsD
         // 1. How do we send Eth to this contract
         require(msg.value > 1e18, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
+        // 18 decimals
 
         // What is reverting?
         // Undo any action that happen  before, and send remaining gas back
 
+    }
+
+    function getPrice() public view returns (uint256) {
+        // ABI
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int price,,,) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        // 3000,00000000
+        return uint256 (price + 1e10); // 1**10 == 10000000000
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversationRate(uint256 ethAmount) public {
+        uint256 ethPrice = getPrice();
+        // 3000_000000000000000000 = ETH / USD price
+        // 1_000000000000000000 ETH
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
     }
         // function withdraw(){}
     
