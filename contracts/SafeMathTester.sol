2626e62d2fb29de74dee5c301d6d3524a2fec1e4
kenny001-spec <abubakarkehinde88@gmail.com>
2024-01-23 14:13:34 +0100

    Safe-Math-Tester.sol...

--- contracts/SafeMathTester.sol
+++ contracts/SafeMathTester.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.0;
 
 contract SafeMathTester {
-    uint8 public bigNumber = 255; // checked
+    uint8 public bigNumber = 255; //checked
 
     function add() public {
         // bigNumber = bigNumber + 1;
         unchecked {bigNumber = bigNumber + 1;}
+        
     }
+    
 }
