f680e077f6570f9215714f8c90defd216f7971f7
kenny001-spec <abubakarkehinde88@gmail.com>
2024-01-25 15:22:00 +0100

    Advance solidity-Immutable & Constant

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,32 +8,35 @@ pragma solidity ^0.8.0;
 
 import "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50 * 1e18; // 1 * 10 ** 18;
+    uint256 public constant MINIMUM_USD = 50 * 1e18; // 1 * 10 ** 18;
 
     address[] public  funders;
     mapping(address => uint256) public addressToAmountFunded;
 
-    address public owner;
-
+    address public immutable i_owner;
+    // 439 gas - Immutable
+    // 2574 gas - Non-Immutable 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
 
     function fund() public payable {
         // Want to be able to start a minimum fund amount in USD
         // 1. How do we send ETH to this contract
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough!");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
     } 
 
         // Basic solidity For Loop
         // A for loop is a way to loop through some type of index object or a range of Numbers
-    function withdraw() public  {
+    function withdraw() public onlyOwner {
 
         /* Starting index, ending index, step amount */  
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex = funderIndex++) {
@@ -57,6 +60,21 @@ contract FundMe {
         // msg.sender = address
         // payable(msg.sender) = payable address
         
+    }
+
+    modifier onlyOwner {
+        if(msg.sender != i_owner) { revert NotOwner(); }
+        // require(msg.sender == i_owner, "Sender is not owner!");
+
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 
     
